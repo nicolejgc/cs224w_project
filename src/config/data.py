fe10525b791f7bc6c0086a76729e4590cc149adf
DavidJGChen <33329617+DavidJGChen@users.noreply.github.com>
2025-12-02 19:44:22 -0800

    ummm idk what changed here but ok

--- src/config/data.py
+++ src/config/data.py
@@ -1,14 +1,7 @@
 DATA_SETTINGS = {
-    'train': {
-        'num_samples': 1000,
-        'length': 16
-    },
-    'val': {
-        'num_samples': 32,
-        'length': 16
-    },
-    'test': {
-        'num_samples': 32,
-        'length': 64
-    }
+    "train": {"num_samples": 1000, "length": 16},
+    "val": {"num_samples": 128, "length": 16},
+    "test": {"num_samples": 128, "length": 16},
+    "test_2x": {"num_samples": 128, "length": 32},
+    "test_4x": {"num_samples": 128, "length": 64},
 }
